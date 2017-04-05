e657664998c5a1d27257da07aba5006c2f33aeaf
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-04 17:27:00 -0700

    Multiple rounds and best score

--- game.py
+++ game.py
@@ -9,13 +9,15 @@ print("Howdy, what's your name?")
 #get player name
 name = raw_input("Type in your name: ")
 #choose random number between 1 and 100
-random_num = random.randint(1,100)
+random_num = random.randint(1, 100)
 print("%s, I'm thinking of a number between 1 and 100" % name)
 print("Try to guess my number")
 
 #repeat forever:
 guess = 0
 num_guesses = 0
+best_score = 0
+
 while guess != random_num:
 #    get guess
     # try-except statement to check for invalid things
@@ -24,16 +26,18 @@ while guess != random_num:
     except ValueError:
         print ("Hey dummy, type a number! Guess again.")
         continue
+
 #    check if guess is int or float
-    if guess.is_integer() == False:
+    if not guess.is_integer():
         print ("Your guess is being rounded down to an integer.")
     guess = int(guess)
 #    increase number of tries
     num_guesses += 1
+
 #    Check for valid guess
     if guess < 1 or guess > 100:
         print ("You git! Guess is out of range 1-100, guess again!")
-        num_guesses -= 1   
+        num_guesses -= 1
 #    if guess is incorrect:
     elif guess > random_num:
         print ("Your guess is too high, try again")
@@ -42,3 +46,17 @@ while guess != random_num:
     else:
 #        congratulate player
         print("Well done, %s! You found my number in %i tries!" % (name, num_guesses))
+
+        if best_score == 0:
+            best_score = num_guesses
+        elif best_score > num_guesses:
+            best_score = num_guesses
+        print("Your best score is %i!" % (best_score))
+        play_again = raw_input("Press 'y' to play again, anything else to quit: ")
+
+        if play_again.lower() == 'y':
+            guess = 0
+            num_guesses = 0
+            random_num = random.randint(1, 100)
+            print("\n\nNEW GAME! I'm thinking of a new number between 1 and 100")
+            print("Try to guess my number")
