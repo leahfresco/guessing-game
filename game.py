7f8728e9ef62ad7e6dd7b43e6d520bd3303831e7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-04 17:46:10 -0700

    Added limit to number of guesses

--- game.py
+++ game.py
@@ -22,6 +22,7 @@ while guess != random_num:
 #    get guess
     # try-except statement to check for invalid things
     try:
+        print("You have %i guesses left" %(7 - num_guesses))
         guess = float(raw_input("Your guess? "))
     except ValueError:
         print ("Hey dummy, type a number! Guess again.")
@@ -39,6 +40,19 @@ while guess != random_num:
         print ("You git! Guess is out of range 1-100, guess again!")
         num_guesses -= 1
 #    if guess is incorrect:
+    elif guess != random_num and num_guesses >= 7:
+        print("That was the wrong number! You ran out of guesses :(")
+        print("Your best score is %i!" % (best_score))
+        play_again = raw_input("Press 'y' to play again, anything else to quit: ")
+
+        if play_again.lower() == 'y':
+            guess = 0
+            num_guesses = 0
+            random_num = random.randint(1, 100)
+            print("\n\nNEW GAME! I'm thinking of a new number between 1 and 100")
+            print("Try to guess my number")
+        else:
+            break
     elif guess > random_num:
         print ("Your guess is too high, try again")
     elif guess < random_num:
